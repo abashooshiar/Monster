99ba5a7b4cfde64d12ef90783d368b5ad3581424
Hooshiar <39864357+abashooshiar@users.noreply.github.com>
2020-11-24 17:56:26 +0100

    Add files via upload
    Dimensionales Array
    Such die Benutzereingabenummer im Array
    Vergleich es mit dem Inhalt des Arrays

--- main.go
+++ main.go
@@ -2,212 +2,70 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"math/rand"
-	"os"
 	"time"
 )
 
 func main() {
+	dimensionsArray := [21][3]int{{},
+		{2, 4, 3}, {1, 5, 6}, {1, 7, 8}, {1, 9, 10}, {2, 10, 11},
+		{2, 7, 12}, {3, 6, 13}, {3, 9, 14}, {4, 8, 15}, {4, 5, 16},
+		{5, 12, 17}, {6, 11, 18}, {7, 14, 18}, {8, 13, 19}, {9, 16, 19},
+		{10, 15, 17}, {11, 16, 20}, {12, 13, 20}, {14, 15, 20}, {17, 18, 19}}
+
 	rand.Seed(time.Now().UnixNano())
 	random := randomInt(1, 20)
-	fmt.Println("Dein aktuelles Zimmer ist : ", random)
-	fmt.Print("Bitte geben Sie den Zielraum : ")
+	fmt.Println("Du befindest dich in Raum : ", random)
+
+	for p := range dimensionsArray {
+		for i := 0; i < len(dimensionsArray); i++ {
+			if random == p {
+				fmt.Println("Angrenzende Räume sind : ", dimensionsArray[p]) //  :  [1,7,8]
+				break
+			}
+		}
+	}
+
+	fmt.Print("Wohin möchtest Du gehen? : ")
 	var j int
 	if _, err := fmt.Scan(&j); err != nil {
 		log.Print("  Scan Destination node ", err)
 		return
 	}
-	Switch(random, j)
 
-	//fmt.Println(j)
+	Finder(random, j, dimensionsArray)
+
 }
 func randomInt(min, max int) int {
 	return min + rand.Intn(max-min)
 }
-func Switch(random, j int) {
-	switch random {
-	case 1:
-		if j == 2 || j == 4 || j == 3 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 2:
-		if j == 1 || j == 5 || j == 6 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 3:
-		if j == 1 || j == 7 || j == 8 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 4:
-		if j == 1 || j == 9 || j == 10 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 5:
-		if j == 2 || j == 10 || j == 11 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 6:
-		if j == 2 || j == 7 || j == 12 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 7:
-		if j == 3 || j == 6 || j == 13 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 8:
-		if j == 3 || j == 9 || j == 14 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 9:
-		if j == 4 || j == 8 || j == 15 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 10:
-		if j == 4 || j == 5 || j == 16 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 11:
-		if j == 5 || j == 12 || j == 17 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 12:
-		if j == 6 || j == 11 || j == 18 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 13:
-		if j == 7 || j == 14 || j == 18 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 14:
-		if j == 8 || j == 13 || j == 19 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 15:
-		if j == 9 || j == 16 || j == 19 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 16:
-		if j == 10 || j == 15 || j == 17 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 17:
-		if j == 11 || j == 16 || j == 20 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 18:
-		if j == 12 || j == 13 || j == 20 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 19:
-		if j == 14 || j == 15 || j == 20 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	case 20:
-		if j == 17 || j == 18 || j == 19 {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Raum "+s+" ist erlaubt.")
-		} else {
-			s := fmt.Sprintf("%d", j)
-			io.WriteString(os.Stdout, "Sie dürfen nicht im Raum "+s+" rein.")
-		}
-		return
-	}
+func Finder(random, j int, dimensional [21][3]int) {
+	for h := range dimensional {
+		for i := 0; i < len(dimensional); i++ {
+			if random == h {
+				//fmt.Println("Angrenzende Räume sind : ", dimensional[h]) //  :  [1,7,8]
+				//fmt.Println("Du befindest dich in Raum:", j)             // j
+				a := dimensional[h]
+				t := FindIn(a[:], j)
+				if t == 1 {
+					fmt.Println("du darfst in Raum", j)
+					return
+				} else {
+					fmt.Println("Du kannst von hier nicht in Raum ", j)
+					return
+				}
 
+			}
+		}
+	}
+}
+func FindIn(a []int, j int) int {
+	t := 0
+	for _, content := range a {
+		if content == j {
+			t = 1
+		}
+	}
+	return t
 }
