da29342de27f241d892f394aa0c682979f468a48
abas <abas.hooshiar@gmail.com>
2020-11-27 14:51:29 +0100

    if-Bedingung hat entfernt , Variablen und funktionen in Kleinbuchstaben  , eine Angrenzend() Funktion hat gelöscht , dimensionsArray [21][3] hat nach dimensionsArray [20][3] geändert.

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"os"
 	"time"
 )
 
 func main() {
-	dimensionsArray := [21][3]int{{},
+	dimensionsArray := [20][3]int{
 		{2, 4, 3}, {1, 5, 6}, {1, 7, 8}, {1, 9, 10}, {2, 10, 11},
 		{2, 7, 12}, {3, 6, 13}, {3, 9, 14}, {4, 8, 15}, {4, 5, 16},
 		{5, 12, 17}, {6, 11, 18}, {7, 14, 18}, {8, 13, 19}, {9, 16, 19},
@@ -16,51 +15,43 @@ func main() {
 
 	rand.Seed(time.Now().UnixNano())
 	random := randomInt(1, 20)
-
-	Angrenzend(random, dimensionsArray)
+	angrenzend(random, dimensionsArray)
 
 }
-func Angrenzend(random int, dimensionsArray [21][3]int) {
+func angrenzend(random int, dimensionsArray [20][3]int) {
 	fmt.Println("Du befindest dich in Raum : ", random)
-	fmt.Println("Angrenzende Räume sind : ", dimensionsArray[random])
+	fmt.Println("Angrenzende Räume sind : ", dimensionsArray[random-1])
 	fmt.Print("Wohin möchtest Du gehen? : ")
 
-	var WohinGehen int
-	if _, err := fmt.Scan(&WohinGehen); err != nil {
-		if !(WohinGehen >= '0' && WohinGehen <= '9') {
-			fmt.Println("Bitte geben Sie eine Nummer ein")
-			Angrenzend(random, dimensionsArray)
-			return
-		}
-		fmt.Fprintln(os.Stderr, err)
+	var wohingehen int
+	if _, err := fmt.Scan(&wohingehen); err != nil {
+		fmt.Print("Bitte geben Sie eine Nummer ein : ")
+		fmt.Scan(&wohingehen)
 	}
-
-	Finder(random, WohinGehen, dimensionsArray)
+	finder(random, wohingehen, dimensionsArray)
 }
 
 func randomInt(min, max int) int {
 	return min + rand.Intn(max-min)
 }
-func Finder(random, WohinGehen int, dimensional [21][3]int) {
+func finder(random, wohingehen int, dimensional [20][3]int) {
 
-	IndexOfDimensional := dimensional[random]
-	sw := FindIn(IndexOfDimensional[:], WohinGehen)
+	indexOfDimensional := dimensional[random-1]
+	sw := findIn(indexOfDimensional[:], wohingehen)
 	if sw == true {
-		random = WohinGehen
-		Angrenzend(random, dimensional)
-		return
+		random = wohingehen
+		angrenzend(random, dimensional)
 	} else {
-		fmt.Println("Du kannst von hier nicht in Raum ", WohinGehen)
-		Angrenzend(random, dimensional)
-		return
+		fmt.Println("Du kannst von hier nicht in Raum ", wohingehen)
+		angrenzend(random, dimensional)
 	}
 }
-func FindIn(DimensionalContent []int, WohinGehen int) bool {
-	SwitchKey := false
-	for _, content := range DimensionalContent {
-		if content == WohinGehen {
-			SwitchKey = true
+func findIn(dimensionalContent []int, wohingehen int) bool {
+	switchKey := false
+	for _, content := range dimensionalContent {
+		if content == wohingehen {
+			switchKey = true
 		}
 	}
-	return SwitchKey
+	return switchKey
 }
